d4cf6eec0b540a9eae2928d00bfe5d9e74eca95a
Dan Popescu <35501848+chinezbrun@users.noreply.github.com>
2020-05-03 11:34:55 +0300

    Create ChangeMateStatus.sh

---
+++ PredictiveData/ChangeMateStatus.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# task from local PI
+/usr/bin/python3 /var/www/html/SolarPowerDiversion/PredictiveData/ChangeMateStatusModBus.py
+# task from NAS 
+#/volume1/@appstore/python3/bin/python3 /volume1/web/SolarPowerDiversion/PredictiveData/ChangeMateStatusModBus.py
